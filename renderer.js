dcb0e4f6f094580e846a0aff8b5334e2fc24dbd1
DongHyun Song <dh81.song@samsung.com>
2020-03-10 14:34:12 +0900

    Storing server info with localStorage
    Signed-off-by: DongHyun Song <songdh418@gmail.com>

--- renderer.js
+++ renderer.js
@@ -1,5 +1,16 @@
 const {ipcRenderer} = require('electron');
 
+var default_expires = new Date();
+default_expires.setFullYear(default_expires.getFullYear()+1);
+
+function getPreference(name) {
+  return localStorage.getItem(name);
+}
+
+function setPreference(name, value) {
+  localStorage.setItem(name, value);
+}
+
 function getListOfFiles() {
     ipcRenderer.send('command', 'ls -al');
 }
@@ -10,10 +21,24 @@ function setServerInfo() {
     var ssh_user = document.getElementById("ssh_user").value;
     var ssh_pass = document.getElementById("ssh_pass").value;
 
+    setPreference("ssh_host", ssh_host);
+    setPreference("ssh_user", ssh_user);
+    setPreference("ssh_pass", ssh_pass);
+
     let ssh_info = {
         "host": ssh_host,
         "user": ssh_user,
         "pass": ssh_pass
     };
     ipcRenderer.send('set-ssh', JSON.stringify(ssh_info));
+}
+
+window.onload = function () {
+  var ssh_host = document.getElementById("ssh_host");
+  var ssh_user = document.getElementById("ssh_user");
+  var ssh_pass = document.getElementById("ssh_pass");
+
+  ssh_host.value = getPreference("ssh_host");
+  ssh_user.value = getPreference("ssh_user");
+  ssh_pass.value = getPreference("ssh_pass");
 }
