46b243aae739ccfe8a1e346e65d5e32d773aec3a
DongHyun Song <dh81.song@samsung.com>
2020-03-11 14:22:45 +0900

    Formatting renderer.js

--- renderer.js
+++ renderer.js
@@ -1,7 +1,7 @@
-const {ipcRenderer} = require('electron');
+const { ipcRenderer } = require('electron');
 
 var default_expires = new Date();
-default_expires.setFullYear(default_expires.getFullYear()+1);
+default_expires.setFullYear(default_expires.getFullYear() + 1);
 
 function getPreference(name) {
   return localStorage.getItem(name);
@@ -12,28 +12,32 @@ function setPreference(name, value) {
 }
 
 function getListOfFiles() {
-    ipcRenderer.send('command', 'ls -al');
+  ipcRenderer.send('command', 'ls -al');
+}
+
+function sendServerInfo(ssh_host, ssh_user, ssh_pass) {
+  let ssh_info = {
+    "host": ssh_host,
+    "user": ssh_user,
+    "pass": ssh_pass
+  };
+  ipcRenderer.send('set-ssh', JSON.stringify(ssh_info));
 }
 
 function setServerInfo() {
-    console.log("setServerInfo()");
-    var ssh_host = document.getElementById("ssh_host").value;
-    var ssh_user = document.getElementById("ssh_user").value;
-    var ssh_pass = document.getElementById("ssh_pass").value;
+  console.log("setServerInfo()");
+  var ssh_host = document.getElementById("ssh_host").value;
+  var ssh_user = document.getElementById("ssh_user").value;
+  var ssh_pass = document.getElementById("ssh_pass").value;
 
-    setPreference("ssh_host", ssh_host);
-    setPreference("ssh_user", ssh_user);
-    setPreference("ssh_pass", ssh_pass);
+  setPreference("ssh_host", ssh_host);
+  setPreference("ssh_user", ssh_user);
+  setPreference("ssh_pass", ssh_pass);
 
-    let ssh_info = {
-        "host": ssh_host,
-        "user": ssh_user,
-        "pass": ssh_pass
-    };
-    ipcRenderer.send('set-ssh', JSON.stringify(ssh_info));
+  sendServerInfo(ssh_host, ssh_user, ssh_pass);
 }
-
-window.onload = function () {
+function loadServerInfo() {
+  console.log("loadServerInfo()");
   var ssh_host = document.getElementById("ssh_host");
   var ssh_user = document.getElementById("ssh_user");
   var ssh_pass = document.getElementById("ssh_pass");
@@ -41,4 +45,8 @@ window.onload = function () {
   ssh_host.value = getPreference("ssh_host");
   ssh_user.value = getPreference("ssh_user");
   ssh_pass.value = getPreference("ssh_pass");
+}
+
+window.onload = function () {
+  loadServerInfo();
 }
